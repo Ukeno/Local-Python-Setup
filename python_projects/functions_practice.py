c2999aa76335aa5460502ff1c5ff01f62e6fdfe8
Kenny K <koablood@gmail.com>
2023-03-16 02:55:47 -0700

    Local Python Setup

---
+++ python_projects/functions_practice.py
@@ -0,0 +1,31 @@
+def hello():
+    print("\n"'nothing') # prints nothing
+    # whitespace
+hello()
+
+print("-------------------")
+
+def pack():
+    s = '-- Second'
+    print(s)
+    # whitespce             main program
+print('First')       # prints before calling pack()
+pack()               # prints (s)
+print('Third')   # prints after calling pack()
+
+print("-------------------")
+
+def eat_lunch(list1):
+  for x in list1:
+    print(x)
+
+list1 = ["First i eat apple", "Next i eat banana", "Last i eat cherry"]
+    # whitespace
+list2 = [] # If list2 is empty it will pass "if" on to "else"
+    # whitespace
+if list2:
+    print("Lunchbox not empty!") 
+else:
+   print("Lunchbox empty!")   
+
+eat_lunch(list2)
